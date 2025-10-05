925a2fb76114f167ac789f9da63fe664690c02e9
Joshua Desroches <josh.des.21@gmail.com>
2025-10-05 10:42:02 -0400

    Added Drag N Drop functionality with prototype Drop Field Slot

--- Assets/Scripts/HoldClickRepeater.cs
+++ Assets/Scripts/HoldClickRepeater.cs
@@ -40,8 +40,55 @@ public class HoldClickRepeater : MonoBehaviour, IPointerDownHandler, IPointerUpH
         // Mark to suppress the next single click (so releasing after a hold doesn't trigger a single increment)
         shop.SuppressNextClick(slotIndex);
 
-        // Perform a single decrement and finish (no repeated calls)
-        shop.DecrementSoldierByIndex(slotIndex);
+        // Spawn a draggable copy of the soldier and begin drag so the player can place it.
+        GameObject dragObj = shop.SpawnSoldierForDrag(slotIndex);
+        if (dragObj != null && EventSystem.current != null)
+        {
+            // Create a PointerEventData at the current mouse position
+            var pe = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition,
+                pressPosition = Input.mousePosition
+            };
+
+            // Raycast to find the actual UI element under the pointer (should include our new object)
+            var results = new System.Collections.Generic.List<RaycastResult>();
+            EventSystem.current.RaycastAll(pe, results);
+
+            if (results.Count > 0)
+            {
+                // Use the topmost raycast hit
+                var top = results[0];
+
+                // Send pointer down to the hit object (this should set pointerPress etc. internally)
+                ExecuteEvents.Execute(top.gameObject, pe, ExecuteEvents.pointerDownHandler);
+
+                // Find an object that can handle drag (IDragHandler) starting from the top hit
+                GameObject dragHandler = ExecuteEvents.GetEventHandler<IDragHandler>(top.gameObject);
+                if (dragHandler != null)
+                {
+                    // Inform EventSystem which object is being dragged
+                    pe.pointerDrag = dragHandler;
+                    // Mark as pressed on this object
+                    pe.pointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(top.gameObject);
+
+                    // Set selected object
+                    EventSystem.current.SetSelectedGameObject(dragHandler);
+
+                    // BeginDrag on the drag handler
+                    ExecuteEvents.Execute(dragHandler, pe, ExecuteEvents.beginDragHandler);
+
+                    // Finally, send an initial drag event so OnDrag receives a delta if any
+                    ExecuteEvents.Execute(dragHandler, pe, ExecuteEvents.dragHandler);
+                }
+                else
+                {
+                    // Fall back: execute begin drag on the top hit's hierarchy
+                    ExecuteEvents.ExecuteHierarchy(top.gameObject, pe, ExecuteEvents.beginDragHandler);
+                    ExecuteEvents.ExecuteHierarchy(top.gameObject, pe, ExecuteEvents.dragHandler);
+                }
+            }
+        }
     }
 
     void StopRepeat()
