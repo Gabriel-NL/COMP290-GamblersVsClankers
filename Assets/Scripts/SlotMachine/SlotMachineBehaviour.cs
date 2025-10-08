183b2dcc437fab2f47e75a0baf197ab2f914f92b
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2025-10-08 16:29:13 -0400

    Refactor slot machine scene and add weighted selection
    -Renamed SampleScene to SlotMachine
    -References in build settings.
    -Refactored SlotMachineBehaviour to use weighted random selection for both character and rarity, introducing new data structures for probabilities and results.
    -Removed unused soldierType array and related scene data.

--- Assets/Scripts/SlotMachine/SlotMachineBehaviour.cs
+++ Assets/Scripts/SlotMachine/SlotMachineBehaviour.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class SlotMachineBehaviour : MonoBehaviour
 {
-    public SoldierType[] soldierType;
+    public CharactherAndProbability[] possibleResults;
+    public RarityAndProbability[] possibleRarities;
+    private CharactherAndRarity[] currentResults = new CharactherAndRarity[3];
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +21,62 @@ public class SlotMachineBehaviour : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public SoldierType WeightedRandomSoldierSelection()
+    {
+        float randomIndex = Random.Range(0.01f, 100.00f);
+
+        foreach (CharactherAndProbability characther in possibleResults)
+        {
+            if (randomIndex <= characther.probability)
+            {
+                return characther.soldierType;
+            }
+        }
+        return possibleResults[possibleResults.Length - 1].soldierType;
+    }
+
+    public SoldierTierList.TierEnum WeightedRaritySelection()
+    {
+        float randomIndex = Random.Range(0.01f, 100.00f);
+        foreach (RarityAndProbability rarity in possibleRarities)
+        {
+            if (randomIndex <= rarity.probability)
+            {
+                return rarity.soldierType;
+            }
+        }
+
+        return (SoldierTierList.TierEnum)(possibleRarities.Length - 1);
+    }
+    public void Spin()
+    {
+        for (int i = 0; i < currentResults.Length; i++)
+        {
+            currentResults[i] = new CharactherAndRarity
+            {
+                soldierType = WeightedRandomSoldierSelection(),
+                tier = WeightedRaritySelection()
+            };
+        }
+    }
+    [System.Serializable]
+    public class CharactherAndProbability
+    {
+        public SoldierType soldierType;
+        public float probability;
+    }
+    [System.Serializable]
+    public class RarityAndProbability
+    {
+        public SoldierTierList.TierEnum soldierType;
+        public float probability;
+    }
+    [System.Serializable]
+    public class CharactherAndRarity
+    {
+        public SoldierType soldierType;
+        public SoldierTierList.TierEnum tier;
     }
 }
