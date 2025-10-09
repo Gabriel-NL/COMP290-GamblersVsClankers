81999f94719c0e6b1af21f27bb675f1eeb35aea4
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2025-10-09 04:24:20 -0400

    Add test soldier prefabs and update slot machine (UI+Script)
    -Added new test soldier variant prefabs (Assault Rifle Soldier, Laser Soldier, Pistol Soldier, Rifle, and SoldierBase).
    -Updated SlotMachine to restructure slot UI elements, (added background images on each slot)
    -Connect slot images and backgrounds to the SlotMachineBehaviour.
    -Updated scriptable objects for soldier types

--- Assets/Scripts/SlotMachine/SlotMachineBehaviour.cs
+++ Assets/Scripts/SlotMachine/SlotMachineBehaviour.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+using Random = UnityEngine.Random;
 
 public class SlotMachineBehaviour : MonoBehaviour
 {
     public CharactherAndProbability[] possibleResults;
     public RarityAndProbability[] possibleRarities;
     private CharactherAndRarity[] currentResults = new CharactherAndRarity[3];
-
+    public Image[] slotMachineImages = new Image[3];
+    public Image[] slotMachineBGImages = new Image[3];
 
 
 
@@ -29,7 +35,7 @@ public class SlotMachineBehaviour : MonoBehaviour
 
         foreach (CharactherAndProbability characther in possibleResults)
         {
-            if (randomIndex <= characther.probability)
+            if (randomIndex >= characther.probability)
             {
                 return characther.soldierType;
             }
@@ -37,29 +43,79 @@ public class SlotMachineBehaviour : MonoBehaviour
         return possibleResults[possibleResults.Length - 1].soldierType;
     }
 
-    public SoldierTierList.TierEnum WeightedRaritySelection()
+    public SoldierTier WeightedRaritySelection()
     {
         float randomIndex = Random.Range(0.01f, 100.00f);
         foreach (RarityAndProbability rarity in possibleRarities)
         {
-            if (randomIndex <= rarity.probability)
+            if (randomIndex >= rarity.probability)
             {
-                return rarity.soldierType;
+
+                return SoldierTierList.tierDictionary[rarity.type];
+            }
+        }
+        SoldierTierList.TierEnum lastType = possibleRarities[possibleRarities.Length - 1].type;
+        return SoldierTierList.tierDictionary[lastType];
+    }
+    [NaughtyAttributes.Button]
+    public void Spin()
+    {
+        slotMachineImages[0].sprite = possibleResults[0].soldierType.characterSprite;
+
+        for (int i = 0; i < currentResults.Length; i++)
+        {
+            SoldierType newSoldier = WeightedRandomSoldierSelection();
+            slotMachineImages[i].sprite = newSoldier.characterSprite;
+
+            SoldierTier newTier = WeightedRaritySelection();
+            slotMachineBGImages[i].color = newTier.tierColor;
+
+            currentResults[i] = new CharactherAndRarity() { soldierType = newSoldier, tier = newTier };
+        }
+
+        CharactherAndRarity rolledCharacther = GetRolledCharacther();
+        Debug.Log($"{rolledCharacther.soldierType.name} - {rolledCharacther.tier.tierType}");
+    }
+
+    public CharactherAndRarity GetRolledCharacther()
+    {
+        CharactherAndRarity endResult = new CharactherAndRarity();
+        endResult.tier = currentResults[0].tier;
+        for (int i = 1; i < currentResults.Length; i++)
+        {
+            if (currentResults[i].tier.tierWeight < endResult.tier.tierWeight)
+            {
+                endResult = currentResults[i];
+            }
+
+        }
+        Dictionary<SoldierType, int> frequency = new Dictionary<SoldierType, int>();
+        foreach (var result in currentResults)
+        {
+            SoldierType soldier = result.soldierType;
+            if (frequency.ContainsKey(soldier))
+            {
+                frequency[soldier]++;
+            }
+            else
+            {
+                frequency[soldier] = 1;
             }
         }
 
-        return (SoldierTierList.TierEnum)(possibleRarities.Length - 1);
-    }
-    public void Spin()
-    {
-        for (int i = 0; i < currentResults.Length; i++)
+        SoldierType mostFrequent = null;
+        int maxCount = 0;
+        foreach (var pair in frequency)
         {
-            currentResults[i] = new CharactherAndRarity
+            if (pair.Value > maxCount)
             {
-                soldierType = WeightedRandomSoldierSelection(),
-                tier = WeightedRaritySelection()
-            };
+                maxCount = pair.Value;
+                mostFrequent = pair.Key;
+            }
         }
+
+        endResult.soldierType = mostFrequent;
+        return endResult;
     }
     [System.Serializable]
     public class CharactherAndProbability
@@ -70,13 +126,13 @@ public class SlotMachineBehaviour : MonoBehaviour
     [System.Serializable]
     public class RarityAndProbability
     {
-        public SoldierTierList.TierEnum soldierType;
+        public SoldierTierList.TierEnum type;
         public float probability;
     }
     [System.Serializable]
     public class CharactherAndRarity
     {
         public SoldierType soldierType;
-        public SoldierTierList.TierEnum tier;
+        public SoldierTier tier;
     }
 }
