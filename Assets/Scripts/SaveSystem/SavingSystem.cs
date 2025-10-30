f3e45299a9905e768a27a8031ee5acde8d2b0926
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2025-10-30 16:06:26 -0400

    Add new enemy prefabs and basic saving system
    -Introduced EnemyTemplate,RoombaV2, SmartFridgeV2, SmartLawnMowerV2, and SmartToasterV2 prefabs for new enemy types.
    -Added a basic SavingSystem script and GameObject to the Gameplay scene for future save/load functionality.
    -Commented out code in JD_EnemyBehaviour.cs that was renaming GameObjects based on enemy type.

---
+++ Assets/Scripts/SaveSystem/SavingSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavingSystem : MonoBehaviour
+{
+
+    [Header("Collection root")]
+    public Transform slotsParent; // <-- target parent exposed in Inspector
+
+    private ItemSlot[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
